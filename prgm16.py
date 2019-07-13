24c0389d100d194738561a19f0ed1ad286de0848
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-13 17:46:52 +0530

    prgm15.py

--- prgm16.py
+++ prgm16.py
@@ -1,7 +1,7 @@
-y,ab=map(int,input().split())
-for i in range(y+1,ab):
-	for n in range(2,i):
-		if(i%n==0):
+x,mn=map(int,input().split())
+for i in range(x+1,mn):
+	for a in range(2,i):
+		if(i%a==0):
 			break
 	else:
 		print(i,end=" ")
