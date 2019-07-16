f9fcbb1b1d4b7b40d16d31e76ffa8fd37fe31fb9
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 16:46:54 +0530

    pro4.py

--- pro4.py
+++ pro4.py
@@ -1,16 +1,12 @@
-m,r=input().split()
-m=int(m)
-r=int(r)
-k1=[]
-l1=input().split()
-1l=[int(i) for i in l1]
-for i in range(r):
-  s1=0
-  u1,v1=input().split()
-  u1=int(u1)
-  v1=int(v1)
-  for i in range(u1-1,v1):
-    m^=l1[i]
-  k.append(s1)
-for i in k1:
-  print(i)
+m,r=map(int,input().split())
+n1=list(map(int,input().split()))
+m1=[]
+for m in range(r):
+       j=list(map(int,input().split()))
+       m1.append(j)
+for i in m1:
+    v1=0
+    for k1 in range(i[0]-1,i[1]):
+        v1=v1^n[k1]
+    print(v1)    
+
