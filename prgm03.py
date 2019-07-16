a846402f2f0ea17ba93b3d0409ee9c4b81e56618
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 15:17:31 +0530

    prgm03.py

--- prgm03.py
+++ prgm03.py
@@ -1,8 +1,28 @@
-p1,a1=input().split()
-h1=abs(len(p)-len(a1))
-for i in range(len(p1)):
-  if len(a1)==1 and a1[i] in p1:
-    break
-  if p1[i]!=a1[i]:
-    h1=h1+1
-print(h1)
+import sys, string, math
+m1,m2 = input().split()
+r1 = len(m1)
+r2 = len(m2)
+if r2 > r1 :
+    i = 0
+    while i<r1 and m1[i] == m2[i] :
+        i += 1
+    print(r2-i)
+elif r2 == r1 :
+    i = 0
+    while i<r2 and m1[i] == m2[i] :
+        i += 1
+    print(r2-i)
+else :
+
+    i = 0
+    while i<r2 and m1[i] == m2[i] :
+        i += 1
+    m31 = m1[i:]
+    m32 = m2[i:]
+    L = list(m31)
+    k = 0
+    for c in m32 :
+        if c in L :
+            k += 1
+            L.remove(c)
+    print(r1-i-k)
