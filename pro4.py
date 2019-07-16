10422e8129e4bf94a2cb29d2719023754537f106
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 16:48:39 +0530

    pro4.py

--- pro4.py
+++ pro4.py
@@ -1,12 +1,13 @@
-m,r=map(int,input().split())
-n1=list(map(int,input().split()))
-m1=[]
-for m in range(r):
-       j=list(map(int,input().split()))
-       m1.append(j)
-for i in m1:
-    v1=0
-    for k1 in range(i[0]-1,i[1]):
-        v1=v1^n[k1]
-    print(v1)    
+a1,b1=map(int,input().split())
+lis=list(map(int,input().split()))[:a]
+res=[]
+while(b1):
+    x1=list(map(int,input().split()))
+    res.append(x1)
+    b1=b1-1   
+for i in res:
+    ans=0
+    for j in range(i[0]-1,i[1]):
+       ans=ans^lis[j]
+    print(ans)
 
