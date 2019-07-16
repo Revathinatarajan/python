686f593781ccb872785b8ee0546b1a299eb9e626
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 16:34:10 +0530

    pro2.py

--- pro2.py
+++ pro2.py
@@ -1,8 +1,5 @@
-a1,b1=map(int,input().split())
-arr1=list(map(int,input().split(None,a)[:a]))
-f=[]
-for i in range(b1):
-  d,g=map(int,input().split())
-  f.append(sum(arr1[d-1:g]))
-for i in f:
-  print(i)
+m,r=list(map(int,input().split()))
+lis1=list(map(int,input().split()))
+for i in range(r):
+  u1,v1=list(map(int,input().split()))
+  print(sum(lis1[u1-1:v1]))
