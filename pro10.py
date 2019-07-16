852e554391adfadb323060d843c9a0727919e86c
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 17:15:20 +0530

    pro10.py

--- pro10.py
+++ pro10.py
@@ -1,11 +1,20 @@
-r,m=list(map(int,input().split()))
-l1=list(map(int,input().split()))
-l1.sort(reverse=True)
-c1=0
-for i in l1:
-  if m==0:
-    break
-  q=m//i
-  c1+=q1
-  m=m-i*(q)
-print(c1)
+a1=input().split()
+t1=int(a1[0])
+con=int(a1[1])
+l1=input().split()
+l1=[int(i) for i in l1]
+l1=sorted(l1,reverse=True)
+temp=0
+count=0
+for i in range(0,len(l1)):
+  while True:
+    if(temp==con):
+      break
+    elif(temp>con):
+      count-=1
+      temp-=l1[i]
+      break
+    elif(temp<con):
+      temp+=l1[i]
+      count+=1
+print(count)
