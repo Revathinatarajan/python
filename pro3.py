cb9219742687d1217b9abc1decd82b9a27e0714d
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 16:39:23 +0530

    pro3.py

--- pro3.py
+++ pro3.py
@@ -1,8 +1,8 @@
-m,r=map(int,input().split())
-arr1=list(map(int,input().split(None,a)[:a]))
-f=[]
-for i in range(r):
-  d,g=map(int,input().split())
-  f.append(min(arr[d-1:g]))
-for i in f:
-  print(i)
+m,r = input().split()
+m,r = int(m), int(r)
+L1 = [ int(x) for x in input().split()]
+for i in range(0,r) :
+    a1,b1 = input().split()
+    a1,b1 = int(a1), int(b1)
+for i in range(0,r):
+    print(min(L1[a1-1:b1]))
