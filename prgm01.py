3721b619cdefc6ef10608e7c67074731d3762c15
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 15:08:13 +0530

    prgm01.py

--- prgm01.py
+++ prgm01.py
@@ -1,18 +1,12 @@
-x1=int(input())
-y1=[]
-for i in range(x):
-  y1.append(input())
-  a1,b1,flag ='',0,False
-for m1 in y1[0]:
-  for n1 in y1[1:]:
-    if len(n1)==b:
-      flag=True
-      break
-    if n1[b1]!=m:
-      break
-  else:
-    a1+=m
-  if flag:
-      break
-  b1+=1
-print(a1)
+p1=int(input())
+o1=[]
+for hr1 in range(0,p1):
+ pan=input()
+ o1.append(pan)
+de=[]
+for hr1 in zip(*o1):
+ if(hr1.count(hr1[0])==len(hr1)):
+  de.append(hr1[0])
+ else:
+  break
+print(''.join(de))
