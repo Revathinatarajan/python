a0581186b86185853202915533e8a1870795d863
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-16 16:52:15 +0530

    pro4.py

--- pro4.py
+++ pro4.py
@@ -1,10 +1,9 @@
 p,q=map(int,input().split())
-lis=list(map(int,input().split()))
+l=list(map(int,input().split()))
 for i in range(q):
-    r,m=map(int,input().split())
-    t1 = l[r-1:m]
-    u1 = t[0]
-    for i in range(1,len(t1)):
-        u1 = u1 ^ t1[i]
-    print(u1)
-
+  r,s=map(int,input().split())
+  t=l[r-1:s]
+  u=t[0]
+  for i in range(1,len(t)):
+    u=u^t[i]
+  print(u)
