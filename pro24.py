5794a212123ee0dd1ab0fbed1ea99d78592e0030
Revathinatarajan <52853405+Revathinatarajan@users.noreply.github.com>
2019-07-17 16:17:12 +0530

    pro24.py

--- pro24.py
+++ pro24.py
@@ -1,9 +1,9 @@
 a1=int(input())
-b1=pow(2,a)
+b1=pow(2,a1)
 z1=[]
 for i in range(b1):  
-    m1=bin(i).replace("0b1","")
+    m1=bin(i).replace("0b","")
     z1.append(m1.zfill(a1))
-    z1.sort(key=(lambda x1:x1.count('1')))
+    z1.sort(key=(lambda x:x.count('1')))
 for i in z1:
     print(i)
